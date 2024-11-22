0eb922659dd3130fac8572bf85190c81e478a490
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2024-11-21 19:23:14 -0700

    Create Variables

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: Karina Chiste
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,7 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
-
+const modelName = document.getElementById("model-text")
+const duration = document.getElementById("duration-text")
 
 
 
